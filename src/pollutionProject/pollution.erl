bc82188f7eec97511414c181b69f37dca51b30ae
olliekrk <krolik.olgierd@gmail.com>
2019-04-09 15:24:16 +0200

    added pollution_server, works but exceptions do not. tests need to be added

--- src/pollutionProject/pollution.erl
+++ src/pollutionProject/pollution.erl
@@ -17,24 +17,28 @@
 -record(measurement, {param, value, datetime}).
 -record(monitor, {locations_map, stations_by_names, data}).
 
-createMonitor() -> #monitor{locations_map = maps:new(), stations_by_names = maps:new(), data = dict:new()}.
+createMonitor() -> #monitor{locations_map = maps:new(), stations_by_names = maps:new(), data = maps:new()}.
 
 getStationByName(Name, M) ->
   try maps:find(Name, M#monitor.stations_by_names) of
     {ok, Station} -> Station
   catch
-    error:_ -> {error, Name, "Station with such name does not exist"}
+    _ -> erlang:throw("Station with such name does not exist.~n")
   end.
 
 getStationByLocation({_, _} = Location, M) ->
   try maps:find(Location, M#monitor.locations_map) of
     {ok, Name} -> getStationByName(Name, M)
   catch
-    error:_ -> {error, Location, "Station with such location does not exist"}
+    _ -> erlang:throw("Station with such location does not exist.~n")
   end.
 
 findMatchingMeasurements(Station, Param, M) ->
-  [Ms || Ms <- dict:fetch(Station, M#monitor.data), Ms#measurement.param == Param].
+  try maps:find(Station, M#monitor.data) of
+    {ok, AllMs} -> [Ms || Ms <- AllMs, Ms#measurement.param == Param]
+  catch
+    _ -> erlang:throw("No matching measurements.~n")
+  end.
 
 findMatchingMeasurements(Station, {{_, _, _}, {_, _, _}} = Datetime, Param, M) ->
   [Ms || Ms <- findMatchingMeasurements(Station, Param, M), Ms#measurement.datetime == Datetime];
@@ -45,118 +49,166 @@ findMatchingMeasurements(Station, {_, _, _} = Date, Param, M) ->
 
 addStation(Name, Location, M) ->
   case (maps:is_key(Location, M#monitor.locations_map) orelse maps:is_key(Name, M#monitor.stations_by_names)) of
-    true -> erlang:throw("Station like that is already registered!");
-    false -> #monitor{
-      locations_map = (M#monitor.locations_map)#{Location => Name},
-      stations_by_names = (M#monitor.stations_by_names)#{Name => #station{name = Name, location = Location}},
-      data = dict:store(#station{name = Name, location = Location}, [], M#monitor.data)}
+    true -> io:format("Station is already registered.~n"), M;
+    false ->
+      Station = #station{name = Name, location = Location},
+      #monitor{
+        locations_map = (M#monitor.locations_map)#{Location => Name},
+        stations_by_names = (M#monitor.stations_by_names)#{Name => Station},
+        data = (M#monitor.data)#{Station=> []}}
   end.
 
 addValue({_, _} = Location, Datetime, Param, Value, M) ->
-  addValueUtil(getStationByLocation(Location, M), #measurement{param = Param, value = Value, datetime = Datetime}, M);
+  try getStationByLocation(Location, M) of
+    Station -> addValueUtil(Station, #measurement{param = Param, value = Value, datetime = Datetime}, M)
+  catch
+    _:Reason -> io:format(Reason), M
+  end;
 
 addValue(Name, Datetime, Param, Value, M) ->
-  addValueUtil(getStationByName(Name, M), #measurement{param = Param, value = Value, datetime = Datetime}, M).
+  try getStationByName(Name, M) of
+    Station -> addValueUtil(Station, #measurement{param = Param, value = Value, datetime = Datetime}, M)
+  catch
+    _:Reason -> io:format(Reason), M
+  end.
 
 addValueUtil(Station, NewMs, M) ->
-  case [Ms || Ms <- dict:fetch(Station, M#monitor.data), Ms#measurement.datetime == NewMs#measurement.datetime, Ms#measurement.param == NewMs#measurement.param] of
-    [] -> M#monitor{data = dict:append(Station, NewMs, M#monitor.data)};
-    _ -> {throw, "Measurement like that already exists", NewMs}
+  AllMs = maps:get(Station, M#monitor.data),
+  case [Ms || Ms <- AllMs, Ms#measurement.datetime == NewMs#measurement.datetime, Ms#measurement.param == NewMs#measurement.param] of
+    [] -> M#monitor{data = (M#monitor.data)#{Station => [NewMs | AllMs]}};
+    _ -> io:format("Measurement is already in the database.~n"), M
   end.
 
 removeValue({_, _} = Location, Datetime, Param, M) ->
-  removeValueUtil(getStationByLocation(Location, M), Datetime, Param, M);
+  try getStationByLocation(Location, M) of
+    Station -> removeValueUtil(Station, Datetime, Param, M)
+  catch
+    _:Reason -> io:format(Reason), M
+  end;
 
 removeValue(Name, Datetime, Param, M) ->
-  removeValueUtil(getStationByName(Name, M), Datetime, Param, M).
+  try getStationByName(Name, M) of
+    Station -> removeValueUtil(Station, Datetime, Param, M)
+  catch
+    _:Reason -> io:format(Reason), M
+  end.
 
 removeValueUtil(Station, Datetime, Param, M) ->
-  M#monitor{data = dict:filter(fun(K, V) ->
-    (K /= Station orelse V#measurement.param /= Param orelse V#measurement.datetime /= Datetime) end, M#monitor.data)}.
+  Filter = fun(K, V) -> K /= Station orelse V#measurement.param /= Param orelse V#measurement.datetime /= Datetime end,
+  M#monitor{data = maps:filter(Filter, M#monitor.data)}.
 
 getOneValue({_, _} = Location, Datetime, Param, M) ->
-  getOneValueUtil(getStationByLocation(Location, M), Datetime, Param, M);
+  try getStationByLocation(Location, M) of
+    Station -> getOneValueUtil(Station, Datetime, Param, M)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end;
 
 getOneValue(Name, Datetime, Param, M) ->
-  getOneValueUtil(getStationByName(Name, M), Datetime, Param, M).
+  try getStationByName(Name, M) of
+    Station -> getOneValueUtil(Station, Datetime, Param, M)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end.
 
 getOneValueUtil(Station, Datetime, Param, M) ->
-  case findMatchingMeasurements(Station, Datetime, Param, M) of
-    [] -> erlang:throw("No matching measurements");
-    [M] -> M
+  try findMatchingMeasurements(Station, Datetime, Param, M) of
+    [SingleMs] -> SingleMs;
+    _ -> {unknown_value, "More than one measurement was found."}
+  catch
+    _:Reason -> {unknown_value, Reason}
   end.
 
 getStationMean({_, _} = Location, Param, M) ->
-  getStationMeanUtil(getStationByLocation(Location, M), Param, M);
+  try getStationByLocation(Location, M) of
+    Station -> getStationMeanUtil(Station, Param, M)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end;
 
 getStationMean(Name, Param, M) ->
-  getStationMeanUtil(getStationByName(Name, M), Param, M).
+  try getStationByName(Name, M) of
+    Station -> getStationMeanUtil(Station, Param, M)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end.
 
 getStationMeanUtil(Station, Param, M) ->
-  case findMatchingMeasurements(Station, Param, M) of
-    [] -> erlang:throw("Unable to calculate stations's mean: No measurements");
-    MsList -> lists:foldl(fun(Ms, Acc) -> Ms#measurement.value + Acc end, 0, MsList) / lists:flatlength(MsList)
+  try findMatchingMeasurements(Station, Param, M) of
+    [] -> 0;
+    MsList ->
+      lists:foldl(fun(Ms, Acc) -> Ms#measurement.value + Acc end, 0, MsList) / length(MsList)
+  catch
+    _:Reason -> {unknown_value, Reason}
   end.
 
 getStationDailyMean(Station, M, DateParamFilter) ->
-  MsList = lists:filter(DateParamFilter, dict:fetch(Station, M#monitor.data)),
-  lists:foldl(fun(Ms, Acc) -> Ms#measurement.value + Acc end, 0, MsList) / lists:flatlength(MsList).
+  try maps:find(Station, M#monitor.data) of
+    AllMs ->
+      MsList = lists:filter(DateParamFilter, AllMs),
+      lists:foldl(fun(Ms, Acc) -> Ms#measurement.value + Acc end, 0, MsList) / lists:flatlength(MsList)
+  catch
+    _ -> erlang:throw("Station does not exist")
+  end.
 
-getDailyMean(Param, Date, M) ->
-  DateParamFilter =
-    fun
-      (Ms) ->
-        {MsDate, _} = Ms#measurement.datetime,
-        Ms#measurement.param == Param andalso MsDate == Date
-    end,
+getDailyMean(Date, Param, M) ->
+  FilterFun = fun(Ms) ->
+    {MsDate, _} = Ms#measurement.datetime,
+    MsDate == Date andalso Ms#measurement.param == Param end,
 
-  StationDailyMean =
-    fun
-      (Station) -> getStationDailyMean(Station, M, DateParamFilter)
-    end,
+  MeanFun = fun(Station) -> getStationDailyMean(Station, M, FilterFun) end,
 
-  Stations = dict:fetch_keys(M#monitor.data),
-  lists:sum(lists:map(StationDailyMean, Stations)) / lists:flatlength(Stations).
+  Stations = maps:keys(M#monitor.data),
+  try
+    lists:sum(lists:map(MeanFun, Stations)) / lists:flatlength(Stations)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end.
 
-calculateGrowth(Ms, NextMs) ->
+getGrowth(Ms, NextMs) ->
   Growth = NextMs#measurement.value - Ms#measurement.value,
   {_, {GrowthHour, _, _}} = NextMs#measurement.datetime,
   {Growth, GrowthHour}.
 
-growthsZipFun([], _) -> [];
-growthsZipFun(_, []) -> [];
-growthsZipFun([Ms | MsTail], [NextMs | NextMsTail]) ->
-  [calculateGrowth(Ms, NextMs) | growthsZipFun(MsTail, NextMsTail)].
+zipToGrowths([], _) -> [];
+zipToGrowths(_, []) -> [];
+zipToGrowths([Ms | MsTail], [NextMs | NextMsTail]) ->
+  [getGrowth(Ms, NextMs) | zipToGrowths(MsTail, NextMsTail)].
 
-getMaximumStationGrowthTime({_, _} = Location, Param, Date, M) ->
-  getMaximumStationGrowthTimeUtil(getStationByLocation(Location, M), Param, Date, M);
+getMaximumStationGrowthTime({_, _} = Location, Date, Param, M) ->
+  try getStationByLocation(Location, M) of
+    Station -> getMaximumStationGrowthTimeUtil(Station, Date, Param, M)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end;
 
-getMaximumStationGrowthTime(Name, Param, Date, M) ->
-  getMaximumStationGrowthTimeUtil(getStationByName(Name, M), Param, Date, M).
+getMaximumStationGrowthTime(Name, Date, Param, M) ->
+  try getStationByName(Name, M) of
+    Station -> getMaximumStationGrowthTimeUtil(Station, Date, Param, M)
+  catch
+    _:Reason -> {unknown_value, Reason}
+  end.
 
-getMaximumStationGrowthTimeUtil(Station, Param, Date, M) ->
-  SortFun =
-    fun(Ms1, Ms2) ->
-      calendar:datetime_to_gregorian_seconds(Ms1#measurement.datetime) =< calendar:datetime_to_gregorian_seconds(Ms2#measurement.datetime)
-    end,
+getMaximumStationGrowthTimeUtil(Station, Date, Param, M) ->
+  ConvertDateTime = fun calendar:datetime_to_gregorian_seconds/1,
+  SortFun = fun(Ms1, Ms2) ->
+    ConvertDateTime(Ms1#measurement.datetime) =< ConvertDateTime(Ms2#measurement.datetime)
+            end,
+  MaxFun = fun
+             ({G1, H1}, {G2, _}) when G1 > G2 -> {G1, H1};
+             ({_, _}, {G2, H2}) -> {G2, H2}
+           end,
 
-  MaxFun =
-    fun({G1, H1}, {G2, H2}) ->
-      case G1 > G2 of
-        true -> {G1, H1};
-        false -> {G2, H2}
-      end
-    end,
-
-  ParamMs = findMatchingMeasurements(Station, Date, Param, M),
-  case ParamMs of
-    [] -> {0, no_growth};
-    [_ | []] -> {0, no_growth};
-    _ ->
-      SortedMs = lists:sort(SortFun, ParamMs),
+  try findMatchingMeasurements(Station, Date, Param, M) of
+    [] -> {zero_growth, no_measurement};
+    [_ | []] -> {zero_growth, one_measurement};
+    MsList ->
+      SortedMs = lists:sort(SortFun, MsList),
       [_ | SortedMsTail] = SortedMs,
-      Growths = growthsZipFun(SortedMs, SortedMsTail),
-      lists:foldl(MaxFun, {0, no_growth}, Growths)
+      Growths = zipToGrowths(SortedMs, SortedMsTail),
+      lists:foldl(MaxFun, {0, 0}, Growths)
+  catch
+    _:Reason -> {unknown_value, Reason}
   end.
 
 test() ->
@@ -169,9 +221,9 @@ test() ->
   P6 = pollution:addValue({19, 19}, {{2019, 4, 2}, {20, 0, 0}}, "PM10", 59, P5),
   P7 = pollution:addValue("Eighteen", {{2019, 4, 2}, {11, 0, 0}}, "PM10", 1099, P6),
 
-  pollution:getMaximumStationGrowthTime({18, 18}, "PM10", {2019, 4, 2}, P7),
-  pollution:getMaximumStationGrowthTime("Nineteen", "PM10", {2019, 4, 2}, P7),
-  pollution:getMaximumStationGrowthTime("Twenty", "PM10", {2019, 4, 2}, P7),
+  pollution:getMaximumStationGrowthTime({18, 18}, {2019, 4, 2}, "PM10", P7),
+  pollution:getMaximumStationGrowthTime("Nineteen", {2019, 4, 2}, "PM10", P7),
+  pollution:getMaximumStationGrowthTime("Twenty", {2019, 4, 2}, "PM10", P7),
 
   pollution:getStationMean({18, 18}, "PM10", P7),
   pollution:getStationMean({19, 19}, "PM10", P7),
---
+++ src/pollutionProject/pollution_server.erl
@@ -0,0 +1,74 @@
+-module(pollution_server).
+-author("olliekrk").
+
+%% pollution
+-import(pollution,
+[createMonitor/0,
+addStation/3,
+addValue/5,
+removeValue/4,
+getOneValue/4,
+getStationMean/3,
+getDailyMean/3,
+getMaximumStationGrowthTime/4,
+test/0
+]).
+
+%% API
+-export([start/0, stop/0, server_loop/1]).
+
+%% initializes pollution server
+init() ->
+  server_loop(createMonitor()).
+
+%% creates a server and returns itd PID
+start() ->
+  Pid = spawn(fun() -> init() end),
+  ResolveFun = fun(server, _, _) -> Pid end,
+  global:register_name(server, Pid, ResolveFun),
+  io:format("Starting the server with PID: ~w.", [Pid]).
+
+%% server shutdown
+stop() ->
+  global:send(server, stop).
+
+%% server loop
+server_loop(M) ->
+  receive
+    {_, add_station, Name, Location} ->
+      server_loop(addStation(Name, Location, M)), ok;
+
+    {_, add_value, Identifier, Datetime, Param, Value} ->
+      server_loop(addValue(Identifier, Datetime, Param, Value, M)), ok;
+
+    {_, remove_value, Identifier, Datetime, Param} ->
+      server_loop(removeValue(Identifier, Datetime, Param, M)), ok;
+
+    {Pid, get_value, Identifier, Datetime, Param} ->
+      Pid ! {response, getOneValue(Identifier, Datetime, Param, M)},
+      server_loop(M);
+
+    {Pid, get_station_mean, Identifier, Datetime} ->
+      Pid ! {response, getStationMean(Identifier, Datetime, M)},
+      server_loop(M);
+
+    {Pid, get_daily_mean, Date, Param} ->
+      Pid ! {response, getDailyMean(Date, Param, M)},
+      server_loop(M);
+
+    {Pid, get_max_growth, Identifier, Date, Param} ->
+      Pid ! {response, getMaximumStationGrowthTime(Identifier, Date, Param, M)},
+      server_loop(M);
+
+    stop ->
+      io:format("Shutting down the server with PID: ~w.~n", [self()]), ok;
+
+    _ ->
+      io:format("Unrecognized request.~n"), server_loop(M)
+
+  after
+    20000 ->
+      io:format("Inactivity for 20 s.~n"),
+      io:format("Shutting down the server with PID: ~w.~n", [self()]), ok
+
+  end.
